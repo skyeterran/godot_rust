efd24bbd39d529951b3a08a78aa1a123a7118f6e
Skye Terran <skyeterran@gmail.com>
2024-12-12 13:29:04 -0800

    Testing props, enums

--- rust/src/player.rs
+++ rust/src/player.rs
@@ -3,9 +3,22 @@ use godot::classes::{
     Sprite2D, ISprite2D,
 };
 
+#[derive(GodotConvert, Var, Export)]
+#[godot(via = GString)]
+enum Emotion {
+    Happy,
+    Sad,
+    Angry,
+    Bored,
+}
+
 #[derive(GodotClass)]
-#[class(base=Sprite2D)]
+#[class(base = Sprite2D)]
 pub struct Player {
+    #[export]
+    name: GString,
+    #[export]
+    state: Emotion,
     speed: f64,
     angular_speed: f64,
     base: Base<Sprite2D>
@@ -17,6 +30,8 @@ impl ISprite2D for Player {
         godot_print!("What's up, world?");
 
         Self {
+            name: GString::from("Cool Name"),
+            state: Emotion::Bored,
             speed: 400.0,
             angular_speed: std::f64::consts::PI,
             base,
